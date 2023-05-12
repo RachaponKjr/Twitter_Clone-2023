0a653068001e622fda27e6be50a4a857c570f8b8
RachaponKjr <rachapon.navy@gmail.com>
2023-05-12 17:51:04 +0700

    12/5/2023

--- client/src/Pages/Home/RightS/Signup/Signup.js
+++ client/src/Pages/Home/RightS/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Signup.css";
 import { GrClose } from "react-icons/gr";
 
@@ -11,7 +11,10 @@ const Signup = () => {
       year: "",
     },
   ]);
+  const [day, setDay] = useState([]);
+  const editday = day
 
+  console.log(editday)
   const data = [
     {
       label: "January",
@@ -62,8 +65,384 @@ const Signup = () => {
       value: "December",
     },
   ];
+  const dayNumber1 = [
+    {
+      nuberday: "1",
+      valueday: "1",
+    },
+    {
+      nuberday: "2",
+      valueday: "3",
+    },
+    {
+      nuberday: "4",
+      valueday: "5",
+    },
+    {
+      nuberday: "6",
+      valueday: "6",
+    },
+    {
+      nuberday: "7",
+      valueday: "7",
+    },
+    {
+      nuberday: "8",
+      valueday: "8",
+    },
+    {
+      nuberday: "9",
+      valueday: "9",
+    },
+    {
+      nuberday: "10",
+      valueday: "10",
+    },
+    {
+      nuberday: "11",
+      valueday: "12",
+    },
+    {
+      nuberday: "13",
+      valueday: "13",
+    },{
+      nuberday: "14",
+      valueday: "14",
+    },
+    {
+      nuberday: "15",
+      valueday: "15",
+    },
+    {
+      nuberday: "16",
+      valueday: "16",
+    },
+    {
+      nuberday: "17",
+      valueday: "17",
+    },
+    {
+      nuberday: "18",
+      valueday: "18",
+    },
+    {
+      nuberday: "19",
+      valueday: "19",
+    },
+    {
+      nuberday: "20",
+      valueday: "20",
+    },
+    {
+      nuberday: "21",
+      valueday: "21",
+    },
+    {
+      nuberday: "22",
+      valueday: "22",
+    },
+    {
+      nuberday: "23",
+      valueday: "23",
+    },
+    {
+      nuberday: "24",
+      valueday: "24",
+    },
+    {
+      nuberday: "25",
+      valueday: "25",
+    },
+    {
+      nuberday: "26",
+      valueday: "26",
+    },
+    {
+      nuberday: "27",
+      valueday: "27",
+    },
+    {
+      nuberday: "28",
+      valueday: "28",
+    },
+    {
+      nuberday: "29",
+      valueday: "29",
+    },
+    {
+      nuberday: "30",
+      valueday: "30",
+    }
+  ];
+  const dayNumber2 = [
+    {
+      nuberday: "1",
+      valueday: "1",
+    },
+    {
+      nuberday: "2",
+      valueday: "3",
+    },
+    {
+      nuberday: "4",
+      valueday: "5",
+    },
+    {
+      nuberday: "6",
+      valueday: "6",
+    },
+    {
+      nuberday: "7",
+      valueday: "7",
+    },
+    {
+      nuberday: "8",
+      valueday: "8",
+    },
+    {
+      nuberday: "9",
+      valueday: "9",
+    },
+    {
+      nuberday: "10",
+      valueday: "10",
+    },
+    {
+      nuberday: "11",
+      valueday: "12",
+    },
+    {
+      nuberday: "13",
+      valueday: "13",
+    },{
+      nuberday: "14",
+      valueday: "14",
+    },
+    {
+      nuberday: "15",
+      valueday: "15",
+    },
+    {
+      nuberday: "16",
+      valueday: "16",
+    },
+    {
+      nuberday: "17",
+      valueday: "17",
+    },
+    {
+      nuberday: "18",
+      valueday: "18",
+    },
+    {
+      nuberday: "19",
+      valueday: "19",
+    },
+    {
+      nuberday: "20",
+      valueday: "20",
+    },
+    {
+      nuberday: "21",
+      valueday: "21",
+    },
+    {
+      nuberday: "22",
+      valueday: "22",
+    },
+    {
+      nuberday: "23",
+      valueday: "23",
+    },
+    {
+      nuberday: "24",
+      valueday: "24",
+    },
+    {
+      nuberday: "25",
+      valueday: "25",
+    },
+    {
+      nuberday: "26",
+      valueday: "26",
+    },
+    {
+      nuberday: "27",
+      valueday: "27",
+    },
+    {
+      nuberday: "28",
+      valueday: "28",
+    },
+    {
+      nuberday: "29",
+      valueday: "29",
+    },
+    {
+      nuberday: "30",
+      valueday: "30",
+    },
+    {
+      nuberday: "31",
+      valueday: "31",
+    }
+  ];
+  const dayNumber3 = [
+    {
+      nuberday: "1",
+      valueday: "1",
+    },
+    {
+      nuberday: "2",
+      valueday: "3",
+    },
+    {
+      nuberday: "4",
+      valueday: "5",
+    },
+    {
+      nuberday: "6",
+      valueday: "6",
+    },
+    {
+      nuberday: "7",
+      valueday: "7",
+    },
+    {
+      nuberday: "8",
+      valueday: "8",
+    },
+    {
+      nuberday: "9",
+      valueday: "9",
+    },
+    {
+      nuberday: "10",
+      valueday: "10",
+    },
+    {
+      nuberday: "11",
+      valueday: "12",
+    },
+    {
+      nuberday: "13",
+      valueday: "13",
+    },{
+      nuberday: "14",
+      valueday: "14",
+    },
+    {
+      nuberday: "15",
+      valueday: "15",
+    },
+    {
+      nuberday: "16",
+      valueday: "16",
+    },
+    {
+      nuberday: "17",
+      valueday: "17",
+    },
+    {
+      nuberday: "18",
+      valueday: "18",
+    },
+    {
+      nuberday: "19",
+      valueday: "19",
+    },
+    {
+      nuberday: "20",
+      valueday: "20",
+    },
+    {
+      nuberday: "21",
+      valueday: "21",
+    },
+    {
+      nuberday: "22",
+      valueday: "22",
+    },
+    {
+      nuberday: "23",
+      valueday: "23",
+    },
+    {
+      nuberday: "24",
+      valueday: "24",
+    },
+    {
+      nuberday: "25",
+      valueday: "25",
+    },
+    {
+      nuberday: "26",
+      valueday: "26",
+    },
+    {
+      nuberday: "27",
+      valueday: "27",
+    },
+    {
+      nuberday: "28",
+      valueday: "28",
+    },
+    {
+      nuberday: "29",
+      valueday: "29",
+    }
+  ];
 
-  const handleChange =  (e) => {
+  /* eslint-disable */
+  useEffect(() => {
+    chackMonth();
+  }, [dataB]);
+  /* eslint-enable */
+
+  function chackMonth() {
+    switch (dataB.month) {
+      case "January":
+        setDay(dayNumber2);
+        break;
+      case "February":
+        setDay(dayNumber3);
+        break;
+      case "March":
+        setDay(dayNumber2);
+        break;
+      case "	April":
+        setDay(dayNumber1);
+        break;
+      case "	May":
+        setDay(dayNumber2);
+        break;
+      case "June":
+        setDay(dayNumber1);
+        break;
+      case "July":
+        setDay(dayNumber2);
+        break;
+      case "August":
+        setDay(dayNumber2);
+        break;
+      case "September":
+        setDay(dayNumber1);
+        break;
+      case "October":
+        setDay(dayNumber2);
+        break;
+      case "November":
+        setDay(dayNumber1);
+        break;
+      case "December":
+        setDay(dayNumber2);
+        break;
+      default:
+        setDay(dayNumber2);
+        break;
+    }
+  }
+  const handleChange = (e) => {
     setDataB({ month: e.target.value });
   };
 
@@ -95,6 +474,11 @@ const Signup = () => {
                       {data.label}
                     </option>
                   );
+                })}
+              </select>
+              <select>
+                {editday.map((editday,index)=>{
+                  return <option key={index} value={editday.valueday}>{editday.nuberday}</option>
                 })}
               </select>
             </div>
